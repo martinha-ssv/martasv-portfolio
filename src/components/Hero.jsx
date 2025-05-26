9c3ae71559cec37a1180426dbe33ccae0ad40ffc
Marta Sofia Vasconcelos <marta.sofia.vasconcelos@gmail.com>
2025-05-26 11:29:56 +0200

    Working prototype

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,23 +2,18 @@ import React from "react";
 
 export default function Hero() {
     return (
-        <div className="flex flex-col gap-4 items-center justify-items-center">
-            <div className="flex flex-col lg:flex-row gap-8 lg:gap-16 items-center ">
-                <img
-                src="/self-portrait.svg"
-                alt="self portrait"
-                className="translate-y-[-5%] w-[160px] md:w-[200px] lg:w-[300px] h-auto transition-transform duration-300 ease-in-out hover:scale-105"
-                />
-                <div className="flex font-display font-bold text-6xl md:text-7xl lg:text-9xl lg:text-left">
-                    <p className="">Hi! I'm <br class="hidden lg:max-xl:block"/><span className="relative z-10 text-transparent bg-clip-text bg-gradient-to-r from-secondary to-primary animate-gradient-x">Marta</span>!</p>
-                </div>
-            </div>   
-            <div className="flex flex-col flex-3 justify-text">
-                    <p>I am a biomedical engineering student who’s passionate about blending science, technology, and creativity.</p>
-                    <p>My work explores everything from brain-computer interfaces and cognitive neuroscience to signal processing and immersive VR experiences.</p>
-                    <p>I love tackling real-world challenges and bringing innovative ideas to life, whether it’s designing human-centered tech or diving deep into the brain’s inner workings. </p>
-                    <p>Take a look around and join me on this journey into the future of health, tech, and human potential!</p>
-                </div>
+        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-[auto_fit-content] gap-4 items-center justify-items-center m-">
+            <div className="font-display font-bold text-5xl md:text-7xl lg:text-9xl lg:text-left md:max-w-[calc(100%)] lg:col-span-1 lg:justify-self-start">
+            <p className="">Hi 👋 <br class="max-xl:block"/> I'm <span className="relative z-10 text-transparent bg-clip-text bg-gradient-to-r from-secondary to-primary animate-gradient-x">Marta</span>!</p>
+            </div>
+            <img
+            src="/self-portrait.svg"
+            alt="self portrait"
+            className="translate-y-[-5%] w-[160px] md:w-[200px] lg:w-[300px] h-auto transition-transform duration-300 ease-in-out hover:scale-105 lg:col-span-1 lg:justify-self-end"
+            />
+            <div className="flex flex-col text-xl flex-3 text-left w-[80%] md:max-w-[calc(theme(maxWidth.md))] lg:col-span-2">
+                <p>I'm a biomedical engineering student, blending science and creativity to push health tech forward.</p>
+            </div>
         </div>
     );
 }
